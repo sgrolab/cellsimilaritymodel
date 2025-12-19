5c6b9226726863625e3add7fec16986ae1e05499
mark-aronson <marksaronson@gmail.com>
2025-12-18 19:09:01 -0500

    add files for fig 6

--- simulations/figure_05/diffTF_time.py
+++ simulations/figure_05/diffTF_time.py
@@ -1,30 +1,27 @@
-# Binding motif initial values 
+# Diffusible Ligand Time simulation 
+import pickle
+import numpy as np 
+from utils import motiffunc as mf
+from utils.config import PROJECT_DIR
 
-import os, pickle, numpy as np 
-os.chdir('/groups/sgro/sgrolab/mark/comp_proj/motifs')
-# os.chdir('//prfs.hhmi.org/sgrolab/mark/comp_proj/motifs')
-import motiffunc as mf
-
-Tcc = 1000
 nCells = 1000
 nCycles = 10
 rng = np.random.default_rng(seed=1000)
 
-prodA = 0.01
-prodB = 0.01
-
-kcatA = 0.1
-kcatB = 0.1
-KM = (kcatA * prodA - kcatB * prodB /2) * Tcc**2
-kprod = 0.01
+Tcc = 1000
+prodA = 10**-1
+prodB = 10**-1
+kbind = 10**-3
+kprod = 10**-1
 
 motherCell = mf.Cell(Tcc,0)
-motherCell.parameterize('cdg',[prodA,prodB,kcatA,kcatB,KM,kprod])
+motherCell.parameterize('diffTF',[prodA,prodB,kbind,kprod])
+motherCell.equilibrate()
 motherCell.run(nCells)
 
 divStates = motherCell.getMotherStates()
 
-concentrations = np.zeros([3,nCells,5,int(nCycles*Tcc/10+1)])
+concentrations = np.zeros([3,nCells,6,int(nCycles*Tcc/10+1)])
 
 for i in range(nCells):
     
@@ -47,7 +44,9 @@ for i in range(nCells):
     rnd1.run(nCycles)
     concentrations[2,i] = rnd1.getMolecules()
 
-os.chdir('/groups/sgro/sgrolab/mark/comp_proj/cdg')
-# os.chdir('//prfs.hhmi.org/sgrolab/mark/comp_proj/timeData')
-with open('motifs_cdg.pickle','wb') as f:
-    pickle.dump([motherCell,concentrations],f,pickle.HIGHEST_PROTOCOL)
+vardsis = np.var(concentrations[0] - concentrations[1],axis=0)
+vardrnd = np.var(concentrations[0] - concentrations[2],axis=0)
+normvar = 1-vardsis/vardrnd
+
+with open(PROJECT_DIR / 'motifs/motifs_diffTF3.pickle','wb') as f:
+    pickle.dump(normvar,f,pickle.HIGHEST_PROTOCOL)
