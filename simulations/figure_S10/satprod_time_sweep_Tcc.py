268e0ca16ad08d49161c26b21696fa01a7d75b23
mark-aronson <marksaronson@gmail.com>
2025-12-18 22:43:16 -0500

    add files for Fig S10

---
+++ simulations/figure_S10/satprod_time_sweep_Tcc.py
@@ -0,0 +1,54 @@
+# Saturated Production Motif: Time Sweep Tcc
+import sys 
+import pickle
+import numpy as np 
+from utils import motiffunc as mf
+from utils.config import PROJECT_DIR
+
+nCells = 1000
+nCycles = 10
+rng = np.random.default_rng(seed=1000)
+
+Tccs = [500,1000,2000,5000]
+Tccindex = int(sys.argv[1])
+Tcc = Tccs[Tccindex]
+
+PprodA = 10**-1
+kcatA = 10**-1
+
+motherCell = mf.Cell(Tcc,0)
+motherCell.parameterize('prodsat',[PprodA,kcatA])
+motherCell.equilibrate(20)
+motherCell.run(nCells)
+
+divStates = motherCell.motherStates[2::]
+
+concentrations = np.zeros([3,nCells,6,int(nCycles*Tcc/10+1)])
+
+for i in range(nCells):
+    
+    sis1state = rng.binomial(divStates[:,i].astype('int'),0.5)
+    sis2state = divStates[:,i] - sis1state
+    rnd1state = rng.binomial(divStates[:,rng.integers(0,nCells)].astype('int'),0.5)
+    
+    sis1 = mf.Cell(Tcc,0)
+    sis1.inherit(motherCell,sis1state)
+    sis1.run(nCycles)
+    concentrations[0,i] = sis1.molecules
+    
+    sis2 = mf.Cell(Tcc,0)
+    sis2.inherit(motherCell,sis2state)
+    sis2.run(nCycles)
+    concentrations[1,i] = sis2.molecules
+    
+    rnd1 = mf.Cell(Tcc,0)
+    rnd1.inherit(motherCell,rnd1state)
+    rnd1.run(nCycles)
+    concentrations[2,i] = rnd1.molecules
+
+vardsis = np.var(concentrations[0] - concentrations[1],axis=0)
+vardrnd = np.var(concentrations[0] - concentrations[2],axis=0)
+normvar = 1-vardsis/vardrnd
+
+with open(PROJECT_DIR / 'satprod/time_Tccsweep/satprod_time_Tccsweep_' + str(Tccindex) + '.pickle','wb') as f:
+    pickle.dump(normvar,f,pickle.HIGHEST_PROTOCOL)
