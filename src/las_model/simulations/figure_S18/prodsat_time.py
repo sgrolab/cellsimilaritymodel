701b3254ba8c0aacc7c2beecb4a9a9e05c4ed854
mark-aronson <marksaronson@gmail.com>
2026-09-07 22:48:22 -0400

    fix (figure_S18): update experiment runner with metadata dictionary, shared analysis and output utils
    to do: run experiment and update plotting script

--- src/las_model/simulations/figure_S18/prodsat_time.py
+++ src/las_model/simulations/figure_S18/prodsat_time.py
@@ -1,57 +1,58 @@
-﻿# Saturated Production Time Run
-import pickle
-import sys
-import numpy as np 
+﻿#TODO: Run simulation 
 
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent.parent))
-
+# Saturated Production Time Run
+from datetime import datetime 
+import numpy as np
 from las_model.utils import motiffunc as mf
 from las_model.utils.config import PROJECT_DIR
+from las_model.utils.analyze import calculate_offspring_similarity_time 
+from las_model.utils.output import save_experiment 
 
-Tcc = 1000
-nCells = 1000
-nCycles = 10
-rng = np.random.default_rng(seed=1000)
+# Experiment metadata 
+metadata = {
+    'experiment_name': 'satprod_time',
+    'experiment_directory': 'satprod/time',
+    'created': datetime.now().isoformat(),
+    'seed': 1000,
+    'nCells': 1000,
+    'nCells_equilibrium': 10,
+    'nCycles': 10,
+    'Tcc': 1000,
+    'varTcc': 0,
+    'circuit': 'prodsat',
+    'PprodA': 10**-1,
+    'kcatA': 10**-1
+}
 
-PprodA = 10**-1
-kcatA = 10**-1
+# Pin random seed 
+rng = np.random.default_rng(seed=metadata['seed'])
 
-motherCell = mf.Cell(Tcc,0)
-motherCell.parameterize('prodsat',[PprodA,kcatA])
-motherCell.equilibrate(10)
-motherCell.run(nCells)
+# Initialize and run Mother Cell
+motherCell = mf.Cell(metadata['Tcc'],metadata['varTcc'],rng)
+motherCell.parameterize(metadata['circuit'],[metadata['PprodA'],metadata['kcatA']])
+motherCell.equilibrate(metadata['nCells_equilibrium'])
+motherCell.run(metadata['nCells'])
 
-divStates = motherCell.getMotherStates()
+# Calculate offspring similarity 
+dsis, drnd, vardsis, vardrnd, normvar = calculate_offspring_similarity_time(motherCell,metadata,rng)
 
-concentrations = np.zeros([3,nCells,6,int(nCycles*Tcc/10+1)])
+# append results 
+results = {
+    'dsis': dsis,
+    'drnd': drnd,
+    'vardsis': vardsis,
+    'vardrnd': vardrnd,
+    'normvar': normvar
+}
 
-for i in range(nCells):
-    print(f'Simulating cell {i}...')
-    
-    sis1state = rng.binomial(divStates[:,i].astype('int'),0.5)
-    sis2state = divStates[:,i] - sis1state
-    rnd1state = rng.binomial(divStates[:,rng.integers(0,nCells)].astype('int'),0.5)
-    
-    sis1 = mf.Cell(Tcc,0)
-    sis1.inherit(motherCell,sis1state)
-    sis1.run(nCycles)
-    concentrations[0,i] = sis1.getMolecules()
-    
-    sis2 = mf.Cell(Tcc,0)
-    sis2.inherit(motherCell,sis2state)
-    sis2.run(nCycles)
-    concentrations[1,i] = sis2.getMolecules()
-    
-    rnd1 = mf.Cell(Tcc,0)
-    rnd1.inherit(motherCell,rnd1state)
-    rnd1.run(nCycles)
-    concentrations[2,i] = rnd1.getMolecules()
+# Stack results 
+results = {k: np.stack(v,axis=0) for k, v in results.items()}
 
-# vardsis = np.var(concentrations[0] - concentrations[1],axis=0)
-# vardrnd = np.var(concentrations[0] - concentrations[2],axis=0)
-# normvar = 1-vardsis/vardrnd
-
-with open(PROJECT_DIR / 'correlationcoef/prodsat_time.pickle','wb') as f:
-    pickle.dump(concentrations,f,pickle.HIGHEST_PROTOCOL)
+# Save results 
+exp_dir = save_experiment(
+    experiment_name=metadata['experiment_name'],
+    data = results,
+    metadata=metadata,
+    base_dir=PROJECT_DIR / metadata['experiment_directory']
+)
+print(f"Experiment saved to f{exp_dir}")
