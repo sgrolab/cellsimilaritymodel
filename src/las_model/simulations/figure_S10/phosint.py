720f897ba56b08ba36fac0edf3eda9be640c5cdb
mark-aronson <marksaronson@gmail.com>
2026-09-07 21:47:02 -0400

    fix (figure_S10): update phosint.py to match motiffunc engine, use metadata dictionary, shared analyze and output utils

--- src/las_model/simulations/figure_S10/phosint.py
+++ src/las_model/simulations/figure_S10/phosint.py
@@ -1,56 +1,79 @@
-﻿# Phosphorylation with Intermediate: Sweep PprodA and PprodB 
-import sys 
-import pickle
+﻿#TODO: Run this for full sweep of PprodA and PprodB values
+
+# Phosphorylation with Intermediate: Sweep PprodA and PprodB 
 import numpy as np 
+from datetime import datetime 
 from las_model.utils import motiffunc as mf
 from las_model.utils.config import PROJECT_DIR
+from las_model.utils.analyze import calculate_division_differences
+from las_model.utils.output import save_experiment 
 
-Tcc = 1000
-nCells = 2000
-rng = np.random.default_rng(seed=1000)
+# Experiment metadata
+metadata = {
+    'experiment_name': 'phos_int_sweep_PprodA_PprodB',
+    'experiment_directory': 'phos_int',
+    'created': datetime.now().isoformat(),
+    'seed': 1000,
+    'nCells': 1000,
+    'nCells_equilibrium': 10,
+    'Tcc': 1000,
+    'varTcc': 0,
+    'circuit': 'phos_int',
+    'PprodAs': list(np.logspace(-3,2,31)),
+    'PprodBs': list(np.logspace(-3,2,31)),
+    'ka': 10**-2,
+    'kb': 10**-4,
+    'kt': 10**-2,
+}
 
-prodAs = np.logspace(-3,2,31)
-prodBs = np.logspace(-3,2,31)
+# Pin random seed 
+rng = np.random.default_rng(seed=metadata['seed'])
 
-prodAindex = int(sys.argv[1])
-prodA = prodAs[prodAindex]
-prodBindex = int(sys.argv[2])
-prodB = prodBs[prodBindex]
+results = None 
+for i, PprodA in enumerate(metadata['PprodAs']):
+    for j, PprodB in enumerate(metadata['PprodBs']):
 
-ka = 10**-2
-kb = 10**-4
-kt = 10**-2
+        print(f"Running simulation for PprodA={PprodA}, PprodB={PprodB}")
 
-motherCell = mf.Cell(Tcc,0)
-motherCell.parameterize('phos_int',[prodA,prodB,ka,kb,kt])
-motherCell.equilibrate()
-motherCell.run(nCells)
+        motherCell = mf.Cell(metadata['Tcc'],metadata['varTcc'],rng)
+        motherCell.parameterize(metadata['circuit'],[PprodA,PprodB,metadata['ka'],metadata['kb'],metadata['kt']])
+        motherCell.equilibrate(metadata['nCells_equilibrium'])
+        motherCell.run(metadata['nCells'])
 
-Aeq = np.mean(motherCell.A/motherCell.V)
-Beq = np.mean(motherCell.B/motherCell.V)
-Ceq = np.mean(motherCell.C/motherCell.V)
-Deq = np.mean(motherCell.D/motherCell.V)
-Eeq = np.mean(motherCell.E/motherCell.V) 
+        # Get mother states and calculate division differences 
+        divStates = motherCell.getMotherStates()
+        dsis, drnd, vardsis, vardrnd, normvar = calculate_division_differences(divStates,rng)
 
-divStates = motherCell.getMotherStates()
+        # Get molecule amounts 
+        molecules = motherCell.getMolecules()
+        means = np.mean(molecules,axis=1)
+        variances = np.var(molecules,axis=1)
 
-dsis = np.zeros([nCells,6])
-drnd = np.zeros([nCells,6])
+        if results is None:
+            nVars = dsis.shape[0]
+            results = {
+                'means': np.zeros((len(metadata['PprodAs']), len(metadata['PprodBs']), nVars)),
+                'variances': np.zeros((len(metadata['PprodAs']), len(metadata['PprodBs']), nVars)),
+                'dsis': np.zeros((len(metadata['PprodAs']), len(metadata['PprodBs']), nVars, metadata['nCells'])),
+                'drnd': np.zeros((len(metadata['PprodAs']), len(metadata['PprodBs']), nVars, metadata['nCells'])),
+                'vardsis': np.zeros((len(metadata['PprodAs']), len(metadata['PprodBs']), nVars)),
+                'vardrnd': np.zeros((len(metadata['PprodAs']), len(metadata['PprodBs']), nVars)),
+                'normvar': np.zeros((len(metadata['PprodAs']), len(metadata['PprodBs']), nVars)),
+            }
 
-for k in range(nCells):
-    cell1 = rng.binomial(divStates[:,k].astype('int'),0.5)
-    cell2 = rng.binomial(divStates[:,rng.integers(0,nCells)].astype('int'),0.5)
-    
-    dsis[k] = divStates[:,k] - 2*cell1
-    drnd[k] = cell1 - cell2
+        results['means'][i, j] = means
+        results['variances'][i, j] = variances
+        results['dsis'][i, j] = dsis
+        results['drnd'][i, j] = drnd
+        results['vardsis'][i, j] = vardsis
+        results['vardrnd'][i, j] = vardrnd
+        results['normvar'][i, j] = normvar
 
-normvarA = 1-np.var(dsis[:,0],axis=0)/np.var(drnd[:,0],axis=0)
-normvarB = 1-np.var(dsis[:,1],axis=0)/np.var(drnd[:,1],axis=0)
-normvarC = 1-np.var(dsis[:,2],axis=0)/np.var(drnd[:,2],axis=0)
-normvarD = 1-np.var(dsis[:,3],axis=0)/np.var(drnd[:,3],axis=0)
-normvarE = 1-np.var(dsis[:,4],axis=0)/np.var(drnd[:,4],axis=0)
-
-with open(PROJECT_DIR / 'prodRateSat/phos_int3/phos_int3_prodA_%.2i_prodB_%.2i.pickle' % (prodAindex,prodBindex),'wb') as f:
-    pickle.dump([prodA,prodB,Aeq,Beq,Ceq,Deq,Eeq,normvarA,normvarB,normvarC,normvarD,normvarE],f,pickle.HIGHEST_PROTOCOL)
-
-
+# Save results 
+exp_dir = save_experiment(
+    experiment_name=metadata['experiment_name'],
+    data = [metadata['PprodAs'], metadata['PprodBs'], results],
+    metadata=metadata,
+    base_dir=PROJECT_DIR / metadata['experiment_directory']
+)
+print(f"Experiment saved to f{exp_dir}")
